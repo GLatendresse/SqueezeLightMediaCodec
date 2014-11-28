e0bd087bb0bd121f88ce61f16b53abb1d150c312
GLatendresse <gabriel.latendresse.gmail.com>
2014-11-28 10:56:01 -0500

    Decoupage et recollage véritablement fonctionnels

--- gti310/tp4/EightXEightBlocksMethods.java
+++ gti310/tp4/EightXEightBlocksMethods.java
@@ -11,23 +11,22 @@ public class EightXEightBlocksMethods {
 		EightXEightBlock[][][] eightXEightBlocksContainer = new EightXEightBlock[3]
 				[eightXEightBlocksContainerLength][eightXEightBlocksContainerLength];
 		
-		for(int i = 0; i < 3; i++) 
-			for(int j = 0; j < imageValues[0].length; j+=8) 
-				for(int k = 0; k < imageValues[0].length; k+=8) 
-					for(int u = 0; u < eightXEightBlocksContainerLength; u++)
-						for(int v = 0; v < eightXEightBlocksContainerLength; v++){
-							int[][] eightXEightBlockMatrix = new int[8][8];
-							for(int x = 0; x < 8; x++)
-								for(int y = 0; y < 8; y++)
-									eightXEightBlockMatrix[x][y] = imageValues[i][j+x][k+y]; 
-									
-							EightXEightBlock eightXEightBlock = new EightXEightBlock(eightXEightBlockMatrix);
+		for(int i = 0; i < 3; i++)
+			for(int j = 0; j < imageValues[0].length; j+=8)
+				for(int k = 0; k < imageValues[0].length; k+=8){
+					int[][] eightXEightBlockMatrix = new int[8][8];
+					for(int x = 0; x < 8; x++)
+						for(int y = 0; y < 8; y++)
+							eightXEightBlockMatrix[x][y] = imageValues[i][j+x][k+y];
+								
+					EightXEightBlock eightXEightBlock = new EightXEightBlock(eightXEightBlockMatrix);
 							
-							eightXEightBlocksContainer[i][u][v] = eightXEightBlock;
+					eightXEightBlocksContainer[i][j/8][k/8] = eightXEightBlock;
 					
-						}
+				}
 						
 		return eightXEightBlocksContainer;
+		
 	}
 	
 	public static int[][][] eightXEightBlocksToImage(EightXEightBlock[][][] eightXEightBlocksContainer){
@@ -37,13 +36,11 @@ public class EightXEightBlocksMethods {
 		int[][][] imageValuess = new int[3][imageValuesLength][imageValuesLength];
 		
 		for(int i = 0; i < 3; i++)
-			for(int u = 0; u < eightXEightBlocksContainer[0].length; u++)
-				for(int v = 0; v < eightXEightBlocksContainer[0].length; v++)
-					for(int j = 0; j < imageValuesLength; j+=8) 
-						for(int k = 0; k < imageValuesLength; k+=8) 
-							for(int x = 0; x < 8; x++)
-								for(int y = 0; y < 8; y++)
-									imageValuess[i][j+x][k+y] = eightXEightBlocksContainer[i][u][v].getEightXEightBlockMatrix()[x][y];
+			for(int j = 0; j < imageValuesLength; j+=8) 
+				for(int k = 0; k < imageValuesLength; k+=8) 
+					for(int x = 0; x < 8; x++)
+						for(int y = 0; y < 8; y++)
+							imageValuess[i][j+x][k+y] = eightXEightBlocksContainer[i][j/8][k/8].getEightXEightBlockMatrix()[x][y];
 		
 		return imageValuess;
 		
